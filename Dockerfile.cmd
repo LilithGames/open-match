b02f10ec5d863721e5d4e74a9686343161be5fb6
will <willwu5047@gmail.com>
2021-06-30 16:51:18 +0800

    feat add leader-election

--- Dockerfile.cmd
+++ Dockerfile.cmd
@@ -20,13 +20,13 @@ ARG IMAGE_TITLE
 
 RUN make "build/cmd/${IMAGE_TITLE}"
 
-FROM gcr.io/distroless/static:nonroot
+FROM golang:1.14.0-buster
 ARG IMAGE_TITLE
 WORKDIR /app/
+COPY --from=builder --chown=nonroot "/go/src/open-match.dev/open-match/build/cmd/${IMAGE_TITLE}/run" "/app/${IMAGE_TITLE}/run"
 
-COPY --from=builder --chown=nonroot "/go/src/open-match.dev/open-match/build/cmd/${IMAGE_TITLE}/" "/app/"
-
-ENTRYPOINT ["/app/run"]
+ENV IMAGE_TITLE ${IMAGE_TITLE}
+CMD /app/${IMAGE_TITLE}/run
 
 # Docker Image Arguments
 ARG BUILD_DATE
